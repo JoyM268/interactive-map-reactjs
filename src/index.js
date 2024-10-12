73e0afded0a2e424ba28dd88bce2f38e24144c1d
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-10-12 22:18:35 +0530

    Revert "Github Pages Fix"
    This reverts commit 67078bf22f9c203cc90e461a698081d6fa4df98a.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<Routes>
 				<Route path="interactive-app-reactjs">
 					<Route index element={<App />} />
 					<Route path=":countryName" element={<App />} />
 				</Route>
 			</Routes>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );
