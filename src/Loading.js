4948ce5c2828d374350e4c6ba75943a40b8b8da1
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2025-07-14 20:03:42 +0530

    update sidebar

--- src/Loading.js
+++ src/Loading.js
@@ -6,7 +6,7 @@ export default function Loading({ rotateColor, circleColor, marginTop }) {
 				borderTopColor: rotateColor,
 				marginTop,
 			}}
-			className={`border-solid border-[10px] rounded-[50%] w-[50px] h-[50px] bg-[rgba(0, 0, 0, 0.001)] animate-spin no-select`}
+			className={`border-solid border-[10px] rounded-[50%] w-[50px] h-[50px] bg-[rgba(0, 0, 0, 0.001)] animate-spin no-select mx-[100px]`}
 		></div>
 	);
 }
